9bce4468f4d35c64b22742fd6505deb2263f34b4
Tsung-Chieh Chen <d12300d@gmail.com>
2024-02-19 12:45:32 -0800

    feat: connect to Documentation_Record and split into RQ1 df

--- EDA/db_testing.py
+++ EDA/db_testing.py
@@ -3,6 +3,8 @@
 
 from pymongo import MongoClient
 
+import pandas as pd
+
 # connect to the database
 client = MongoClient('localhost', 27017)
 
@@ -10,16 +12,42 @@ client = MongoClient('localhost', 27017)
 db = client['GithubRepo']
 
 # List all the collections in the 'GithubRepo' database
-print(db.list_collection_names())
+print("All collections: ", db.list_collection_names())
 
 # Connect to the 'Documentation_Record' collection
 Documentation_Record = db['Documentation_Record']
-print(Documentation_Record.find_one())
+print("Number of Records in Documentation_Record: ", Documentation_Record.count_documents({}))
+print()
+
+# print("Documentation_Record: ", Documentation_Record.find_one())
+print("Documentation_Record: ")
+
+# print all the records in the 'Documentation_Record' collection
+# read the records in the 'Documentation_Record' collection into a pandas dataframe
+
+# create a dataframe to store the records in the 'Documentation_Record' collection
+df = pd.DataFrame(list(Documentation_Record.find()))
+print(df)
+print()
+
+# seperate df into popular_df and unpopular_df
+popular_df = df.iloc[:30]
+unpopular_df = df.iloc[30:]
+# popular_df = df[df['popularity'] == 'popular']
+# unpopular_df = df[df['popularity'] == 'unpopular']
+
+print("popular_df: ", popular_df)
+print()
+print("unpopular_df: ", unpopular_df)
 
 # Connect to the 'reposInfo' collection
 reposInfo = db['reposInfo']
-print(reposInfo.find_one())
+print("reposInfo: ", reposInfo.find_one())
 
 # Connect to the 'JavaScript_top500' collection
 # JavaScript_top500 = db['JavaScript_top500']
 # print(JavaScript_top500.find_one())
+
+# Connect to the 'JavaScript_60' collection
+JavaScript_60 = db['JavaScript_60']
+print("JavaScript_60: ", JavaScript_60.find_one())
